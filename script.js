fcb1f04d34a76d401eb3d1e38cb0af826dc87d78
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-11-19 14:26:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const parseCode = (str) => {
   // your code here
+	
+	const parts = str.split('0');
+
+  // Extract values for firstName, lastName, and id
+	let arr=[]
+	for(let i=0; i<parts.length; i++){
+		if(parts[i]!=0){
+			arr.push(parts[i]);
+		}
+	}
+	console.log(arr);
+  const firstName = arr[0];
+  const lastName = arr[1];
+  const id = arr[2];
+
+
+  // Return an object with the extracted values
+  return {
+    firstName: firstName,
+    lastName: lastName,
+    id: id
+  };
 };
 
 // Do not change the code below
